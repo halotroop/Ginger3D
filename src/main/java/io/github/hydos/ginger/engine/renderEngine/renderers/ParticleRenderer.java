6f873a81a8595e49ad0b55ed79efc4006071e816
BuildTools <haydenv06@gmail.com>
2019-05-27 19:48:06 +1000

    cleaning up shaders lol

--- src/main/java/io/github/hydos/ginger/engine/renderEngine/renderers/ParticleRenderer.java
+++ src/main/java/io/github/hydos/ginger/engine/renderEngine/renderers/ParticleRenderer.java
@@ -39,10 +39,9 @@ public class ParticleRenderer {
 		for(ParticleTexture texture : particles.keySet()) {
 			GL13.glActiveTexture(GL13.GL_TEXTURE0);
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture.getTextureID());
+			shader.loadNumberOfRows(texture.getNumberOfRows());
 			for(Particle particle : particles.get(texture)) {
 				updateModelViewMatrix(particle.getPosition(), particle.getRotation(), particle.getScale().x, viewMatrix);
-				shader.loadTextureCoordInfo(particle.getTexOffset1(), particle.getTexOffset2(), texture.getNumberOfRows(), particle.getBlend());
-				GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, quad.getVertexCount());
 			}
 		}
 
--- src/main/java/io/github/hydos/ginger/engine/renderEngine/shaders/ParticleShader.java
+++ src/main/java/io/github/hydos/ginger/engine/renderEngine/shaders/ParticleShader.java
@@ -1,18 +1,15 @@
 package io.github.hydos.ginger.engine.renderEngine.shaders;
 
 import io.github.hydos.ginger.engine.mathEngine.matrixes.Matrix4f;
-import io.github.hydos.ginger.engine.mathEngine.vectors.Vector2f;
 
 public class ParticleShader extends ShaderProgram {
 
 	private static final String VERTEX_FILE = "particleVertexShader.glsl";
 	private static final String FRAGMENT_FILE = "particleFragmentShader.glsl";
 
-	private int location_modelViewMatrix;
+	private int location_numberOfRows;
 	private int location_projectionMatrix;
-	private int location_texOffset1;
-	private int location_texOffset2;
-	private int location_texCoordInfo;
+
 
 	public ParticleShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -20,30 +17,23 @@ public class ParticleShader extends ShaderProgram {
 
 	@Override
 	protected void getAllUniformLocations() {
-		location_modelViewMatrix = super.getUniformLocation("modelViewMatrix");
+		location_numberOfRows = super.getUniformLocation("numberOfRows");
 		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
-		location_texOffset1 = super.getUniformLocation("texOffset1");
-		location_texOffset2 = super.getUniformLocation("texOffset2");
-		location_texCoordInfo = super.getUniformLocation("texCoordInfo");
+
 	}
 
 	@Override
 	protected void bindAttributes() {
 		super.bindAttribute(0, "position");
 	}
-
-	public void loadModelViewMatrix(Matrix4f modelView) {
-		super.loadMatrix(location_modelViewMatrix, modelView);
+	
+	public void loadNumberOfRows(float numberOfRows) {
+		super.loadFloat(location_numberOfRows, numberOfRows);
 	}
 
 	public void loadProjectionMatrix(Matrix4f projectionMatrix) {
 		super.loadMatrix(location_projectionMatrix, projectionMatrix);
 	}
-	
-	public void loadTextureCoordInfo(Vector2f offset1, Vector2f offset2, float numRows, float blend) {
-		super.load2DVector(location_texOffset1, offset1);
-		super.load2DVector(location_texOffset2, offset2);
-		super.load2DVector(location_texCoordInfo, new Vector2f(numRows, blend));
-	}
+
 
 }
