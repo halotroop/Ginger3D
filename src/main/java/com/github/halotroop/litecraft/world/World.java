56503a0ac27804794cb6692b52775d26a9cddc41
valoeghese <valoeghesemods@gmail.com>
2020-02-25 22:23:35 +1300

    world

---
+++ src/main/java/com/github/halotroop/litecraft/world/World.java
@@ -0,0 +1,10 @@
+package com.github.halotroop.litecraft.world;
+
+import com.github.halotroop.litecraft.types.block.Block;
+
+public class World //implements TileAccess
+{
+	public World(long seed)
+	{
+	}
+}
